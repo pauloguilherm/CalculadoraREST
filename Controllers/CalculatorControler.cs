07642eb00c5026ac6f2cefdc67b523ab5f299749
Paulo Guilherme <paulo.guilherme.arruda1@gmail.com>
2022-03-21 21:03:38 -0300

    inicio de logica

---
+++ Controllers/CalculatorControler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Calculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorControler : ControllerBase
+    {
+        private readonly ILogger<CalculatorControler> _logger;
+
+        public CalculatorControler(ILogger<CalculatorControler> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {   
+            if(isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = ConvertTo(firstNumber) + ConvertTo(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private int ConvertTo(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool isNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
