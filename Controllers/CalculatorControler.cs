fc05225c8b6ad0ffefa74dc51cfd88ba50ecf954
Paulo Guilherme <paulo.guilherme.arruda1@gmail.com>
2022-03-21 21:38:05 -0300

    outras operacoes implementadas

--- Controllers/CalculatorControler.cs
+++ Controllers/CalculatorControler.cs
@@ -5,7 +5,7 @@ using System;
 namespace Calculator.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Calculator")]
     public class CalculatorControler : ControllerBase
     {
         private readonly ILogger<CalculatorControler> _logger;
@@ -16,25 +16,102 @@ namespace Calculator.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {   
             if(isNumeric(firstNumber) && isNumeric(secondNumber))
             {
-                var sum = ConvertTo(firstNumber) + ConvertTo(secondNumber);
-                return Ok(sum.ToString());
+                var operacao = ConvertTo(firstNumber) + ConvertTo(secondNumber);
+                return Ok(operacao.ToString());
             }
 
             return BadRequest("Invalid Input");
         }
 
-        private int ConvertTo(string firstNumber)
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+
+        public IActionResult Sub(string firstNumber, string secondNumber)
         {
-            throw new NotImplementedException();
+            if(isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var operacao = ConvertTo(firstNumber) - ConvertTo(secondNumber);
+                return Ok(operacao.ToString());
+            }
+
+            return BadRequest("Invalid Input");
         }
 
-        private bool isNumeric(string firstNumber)
+        [HttpGet("Multi/{firstNumber}/{secondNumber}")]
+
+        public IActionResult Multi(string firstNumber, string secondNumber)
         {
-            throw new NotImplementedException();
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var operacao = ConvertTo(firstNumber) * ConvertTo(secondNumber);
+                return Ok(operacao.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("Divi/{firstNumber}/{secondNumber}")]
+
+        public IActionResult Divi(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var operacao = ConvertTo(firstNumber) / ConvertTo(secondNumber);
+                return Ok(operacao.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("Media/{firstNumber}/{secondNumber}")]
+
+        public IActionResult Media(string firstNumber, string secondNumber)
+        {
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var operacao = ConvertTo(firstNumber) + ConvertTo(secondNumber);
+                var media = operacao / 2;
+                return Ok(media.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("squareroot/{firstNumber}")]
+
+        public IActionResult SquareRoot(string firstNumber)
+        {
+            if (isNumeric(firstNumber))
+            {
+                var operacao = Math.Sqrt((double)ConvertTo(firstNumber));
+                return Ok(operacao.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(
+                strNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+
+            return isNumber;
+        }
+
+        private decimal ConvertTo(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
         }
     }
 }
